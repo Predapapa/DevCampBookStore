205bffd6d9c71e6ff9ec334ec511949a9c5d85ae
Patrik Balazs <patrik.balazs@sap.com>
2020-04-25 16:56:45 +0200

    List of all available Books

--- Devcamp Bookshop StePat/src/main/resources/static/controller/MainView.controller.js
+++ Devcamp Bookshop StePat/src/main/resources/static/controller/MainView.controller.js
@@ -1,11 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 
 	return Controller.extend("BWATC.BookstoreWebAppTC.controller.MainView", {
 		onInit: function () {
+			
+		},
 
-		}
-	});
+		onListItemPress: function (oEvent) {
+			var listItem = oEvent.getParameter("listItem");
+			  var getTitle = listItem.getTitle();
+			  
+		}	});
 });
